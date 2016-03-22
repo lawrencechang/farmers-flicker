8893fbc2115b21adf5e485479e11c4562d363cca
Lawrence Chang <lawrencechangbc@gmail.com>
2016-03-22 16:21:41 -0700

    Initial commit.

--- farmersflicker/ViewController.swift
+++ farmersflicker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("This is my Farmer's Flicker demo app!")
     }
 
     override func didReceiveMemoryWarning() {
